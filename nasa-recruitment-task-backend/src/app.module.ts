71e5c5e4d331d07b982c892ea38cabcce9edd41c
exomat <exomat@wp.pl>
2021-05-24 22:22:36 +0200

    Init nestjs backend app

---
+++ nasa-recruitment-task-backend/src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+
+import { WeatherModule } from './weather/weather.module';
+
+@Module({
+  imports: [WeatherModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
---
+++ nasa-recruitment-task-backend/src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Nasa info recruitment task')
+    .setDescription('Nasa API')
+    .setVersion('1.0')
+    .addTag('nasa')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
